e8b80d3b7643f92198ac2f6ddda047931a50fe71
A2 <14343076+fayac@user.noreply.gitee.com>
2026-02-10 21:03:56 +0800

    更新配置文件，修正链接路径并添加用户协议文档

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -7,14 +7,14 @@ export default {
       {
         text: '入门指南',
         items: [
-          { text: '首次进服指南', link: '/首次进服指南' },
+          { text: '首次进服指南', link: '/Guide' },
           // 可以继续添加其他入门相关文档
         ]
       },
       {
         text: '服务条款',
         items: [
-          { text: 'Chocolate 服务器玩家用户协议与服务条款', link: '/Chocolate 服务器玩家用户协议与服务条款' },
+          { text: 'Chocolate 服务器玩家用户协议与服务条款', link: '/terms' },
           // 可以继续添加其他入门相关文档
         ]
       },
