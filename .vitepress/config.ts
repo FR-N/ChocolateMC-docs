b0c086bd13c07b95c004021cc9ac0090390e2ccf
A2 <14343076+fayac@user.noreply.gitee.com>
2026-02-10 21:08:40 +0800

    fix

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -7,7 +7,7 @@ export default {
       {
         text: '入门指南',
         items: [
-          { text: '首次进服指南', link: '/Guide' },
+          { text: '首次进服指南', link: '/guide' },
           // 可以继续添加其他入门相关文档
         ]
       },
